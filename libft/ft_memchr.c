444a05c265d9f98d97c3a76c49da580e1bf1153d
bastien <bastien.poyet@outlook.fr>
2023-11-07 21:45:02 +0100

    function memcmp.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,39 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bpoyet <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: bpoyet <bpoyet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:24:47 by bpoyet            #+#    #+#             */
-/*   Updated: 2023/11/07 17:45:41 by bpoyet           ###   ########.fr       */
+/*   Updated: 2023/11/07 21:01:48 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <stdio.h>
 
-void *memchr(const void *memoryBlock, int searchedChar, size_t size)
-{
-    char *ptr;
-    ptr = (char *)memoryBlock;
-    printf("ptr[1] %d\n", ptr[0]);
-    while (size-- > 0)
-    {
-        printf("ptr %c et searchedchar %d\n", *ptr, searchedChar);
-        if (*ptr == (char)searchedChar)
-            return ((void *)memoryBlock);
-        ptr++;
-    }
-    return (0);
-}
-int main()
-{
-    char data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    const unsigned int size = 10;
+// #include <stdio.h>
 
-    // On recherche une valeur inhéxistante :
-    void *found = memchr(data, 500, size);
-    if (found == NULL)
-        printf("aucune correspondance\n");
-    else
-        printf("une correspondqnce");
+void	*memchr(const void *str, int c, size_t size)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)str;
+	while (size-- > 0)
+	{
+		if (*ptr == (unsigned char)c)
+			return ((void *)str);
+		ptr++;
+	}
+	return (0);
 }
+// int main()
+// {
+//     char data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+//     const unsigned int size = 10;
+
+//     // On recherche une valeur inhéxistante :
+//     void *found = memchr(data, 60, size);
+//     if (found == NULL)
+//         printf("aucune correspondance\n");
+//     else
+//         printf("une correspondqnce");
+// }
---
+++ libft/ft_memcmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpoyet <bpoyet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 21:08:46 by bpoyet            #+#    #+#             */
+/*   Updated: 2023/11/07 21:36:49 by bpoyet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+// #include <stdio.h>
+
+int	ft_memcmp(const void *ptr1, const void *ptr2, size_t size)
+{
+	char	*cpptr1;
+	char	*cpptr2;
+
+	cpptr1 = (char *)ptr1;
+	cpptr2 = (char *)ptr2;
+	while (size-- > 0 && *cpptr1 == *cpptr2)
+	{
+		cpptr1++;
+		cpptr2++;
+	}
+	return (*cpptr1 - *cpptr2);
+}
+// int main(void)
+// {
+//     int array1 [] = { 54, 85, 20, 63, 21 };
+//     int array2 [] = { 54, 85, 20, 63, 20 };
+//     size_t size = sizeof( int ) * 5;
+//     printf("%d\n",ft_memcmp(array1,array2,size));
+//     printf("%d\n",memcmp(array1,array2,size));
+//     return (0);
+// }
