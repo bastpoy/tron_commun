ccf8787c06ebbc3c31cf32d20cd4161fe857b232
bastien <bastien.poyet@outlook.fr>
2023-12-03 22:26:23 +0100

    last puch norminette correction

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,101 +6,22 @@
 /*   By: bpoyet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:03:14 by bpoyet            #+#    #+#             */
-/*   Updated: 2023/12/03 19:50:54 by bpoyet           ###   ########.fr       */
+/*   Updated: 2023/12/03 22:23:59 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
 #include <stdio.h>
 
-// size_t	ft_getindexbackn(char *temp, int nextbackn)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while ((temp[i] != '\n' || nextbackn) && temp[i] != '\0')
-// 	{
-// 		i++;
-// 	}
-// 	if (temp[i] == '\n')
-// 	{
-// 		i++;
-// 	}
-// 	return (i);
-// }
-
-char *ft_strdup(const char *src, int freesrc, int nextbackn)
+char	*ft_checkoverflow(char *overflow, char *buffer)
 {
-	size_t i;
-	char *dest;
-
-	if (!src || *src == '\0')
-		return (NULL);
-	i = ft_strlen((char *)src, nextbackn);
-	dest = malloc((i + 1) * sizeof(char));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (src[i] && (src[i] != '\n' || nextbackn))
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	if (src[i] == '\n')
-	{
-		dest[i] = '\n';
-		i++;
-	}
-	dest[i] = '\0';
-	if (freesrc)
-		free((char *)src);
-	return (dest);
-}
-
-char *ft_strchr(const char *s, int c)
-{
-	unsigned char *str;
-
-	str = (unsigned char *)s;
-	if (!s)
-	{
-		return (NULL);
-	}
-	while (*str != (unsigned char)c)
-	{
-		if (*str == '\0')
-			return (NULL);
-		str++;
-	}
-	if (*str == '\n')
-		return ((char *)str);
-	return ((char *)str);
-}
-
-char *ft_free(char *overflow, char *temp, int argument)
-{
-	if (argument == 1)
-	{
-		free(overflow);
-		overflow = NULL;
-	}
-	if (argument == 2)
-	{
-		free(temp);
-		temp = NULL;
-	}
-	return (NULL);
-}
-
-char *ft_checkoverflow(char *overflow, char *buffer)
-{
-	char *temp;
+	char	*temp;
 
 	temp = NULL;
 	if (overflow)
 	{
-		if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n') + 1) != '\0')
+		if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n')
+				+ 1) != '\0')
 		{
 			temp = ft_strdup(overflow, NO_FREE, NEXTBACKN);
 			buffer = ft_strjoin(buffer, temp, FREE);
@@ -113,16 +34,18 @@ char *ft_checkoverflow(char *overflow, char *buffer)
 	return (buffer);
 }
 
-char *ft_nextoverflow(char *overflow)
+char	*ft_nextoverflow(char *overflow)
 {
-	char *temp;
+	char	*temp;
 
 	if (overflow)
 	{
-		if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n') + 1) != '\0')
+		if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n')
+				+ 1) != '\0')
 		{
 			temp = ft_strdup(overflow, FREE, NONEXTBACKN);
-			overflow = ft_strdup(ft_strchr(temp, '\n') + 1, NO_FREE, NONEXTBACKN);
+			overflow = ft_strdup(ft_strchr(temp, '\n') + 1, NO_FREE,
+					NONEXTBACKN);
 			free(temp);
 			return (overflow);
 		}
@@ -136,36 +59,32 @@ char *ft_nextoverflow(char *overflow)
 	return (NULL);
 }
 
-char *ft_readline(char *buffer, int fd)
+char	*ft_readline(char *buffer, int fd)
 {
-	int byteread;
-	char *temp;
+	int		byteread;
+	char	*temp;
 
 	byteread = BUFFER_SIZE;
 	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	// printf("re");
 	while (!ft_strchr(buffer, '\n') && byteread != 0)
 	{
-		// printf("pouet");
 		byteread = read(fd, temp, BUFFER_SIZE);
 		if (byteread == -1)
 		{
 			free(temp);
-			return NULL;
+			return (NULL);
 		}
-		if (byteread != 0) // si il vaut 0 jecris pas dans mon buffer;
+		if (byteread != 0)
 		{
 			temp[byteread] = '\0';
-			// printf("temp -%s-\n",temp);
 			buffer = ft_strjoin(buffer, temp, NO_FREE);
 		}
 	}
-	// printf("la");
 	free(temp);
 	return (buffer);
 }
 
-char *ft_modifyoverflow(char *buffer, char *overflow)
+char	*ft_modifyoverflow(char *buffer, char *overflow)
 {
 	if (ft_strchr(buffer, '\n') && *(ft_strchr(buffer, '\n') + 1) != '\0')
 	{
@@ -180,61 +99,52 @@ char *ft_modifyoverflow(char *buffer, char *overflow)
 	}
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	char *buffer;
-	static char *overflow = NULL;
+	char		*buffer;
+	static char	*overflow = NULL;
 
 	buffer = NULL;
 	if (fd < 0 || read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
-		return (overflow = ft_free(overflow, NULL, 1));
-	// si jai overflow je le mets avant buffer et je return buffer	 entree: buffer/over     sortie: buffer => return buffer avec nouveau over sinon rien si presence \n fin de fonctions
-	//  printf("\noverflowok -%s-", overflow);
+	{
+		free(overflow);
+		return (overflow = NULL);
+	}
 	buffer = ft_checkoverflow(overflow, buffer);
-	// printf("buffer apres over -%s-\n", buffer);
-	// modification overflow et attribution de nouvelle valeur		 entree: overflow 		sortie: overflow => null si dans buffer overflow si presence dun \n
 	overflow = ft_nextoverflow(overflow);
-	// printf("overflow change -%s-\n", overflow);
-	// je lis tant que j'ai pas un retour a la ligne ou de fin     	 entree: buffer 		sortie: buffer => buffer avec des elements du texte
 	if (!overflow && !buffer)
 	{
 		buffer = ft_readline(buffer, fd);
-		// printf("buffer1 -%s-\n",buffer);
-		// je stocke la valeur en trop si il y en a une dans mon over	 entree: buffer/over 	sortie: over => null si pas over sinon over
 		overflow = ft_modifyoverflow(buffer, overflow);
-		// printf("%d\n", (int)ft_strlen((char *)overflow, NONEXTBACKN));
-		// printf("overflow -%s-", overflow);
 		buffer = ft_strdup(buffer, FREE, NEXTBACKN);
-		// printf("buffer2 -%s-\n",buffer);
 	}
-	// printf("fin buffer -%s-\n", buffer);
 	return (buffer);
 }
 
-int main(void)
-{
-	int fd;
-	char *str;
+// int	main(void)
+// {
+// 	int fd;
+// 	char *str;
 
-	fd = open("text.a", O_RDONLY);
-	str = get_next_line(fd);
-	// printf("%s", str);
-	// free(str);
-	// str = get_next_line(fd);
-	// printf("%s", str);
-	// free(str);
-	// close(fd);
-	// str = get_next_line(fd);
-	// printf("%s", str);
-	// free(str);
-	// close(fd);
-	// fd = open("text.a", O_RDONLY);
-	while (str)
-	{
-		printf("%s", str);
-		free(str);
-		str = get_next_line(fd);
-	}
-	close(fd);
-	free(str);
-}
+// 	fd = open("text.a", O_RDONLY);
+// 	str = get_next_line(fd);
+// 	// printf("%s", str);
+// 	// free(str);
+// 	// str = get_next_line(fd);
+// 	// printf("%s", str);
+// 	// free(str);
+// 	// close(fd);
+// 	// str = get_next_line(fd);
+// 	// printf("%s", str);
+// 	// free(str);
+// 	// close(fd);
+// 	// fd = open("text.a", O_RDONLY);
+// 	while (str)
+// 	{
+// 		printf("%s", str);
+// 		free(str);
+// 		str = get_next_line(fd);
+// 	}
+// 	close(fd);
+// 	free(str);
+// }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,15 +6,63 @@
 /*   By: bpoyet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:19:11 by bpoyet            #+#    #+#             */
-/*   Updated: 2023/12/01 12:24:41 by bpoyet           ###   ########.fr       */
+/*   Updated: 2023/12/03 22:25:57 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str, int nextbackn)
+char *ft_strdup(const char *src, int freesrc, int nextbackn)
 {
-	size_t	i;
+	size_t i;
+	char *dest;
+
+	if (!src || *src == '\0')
+		return (NULL);
+	i = ft_strlen((char *)src, nextbackn);
+	dest = malloc((i + 1) * sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i] && (src[i] != '\n' || nextbackn))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (src[i] == '\n')
+	{
+		dest[i] = '\n';
+		i++;
+	}
+	dest[i] = '\0';
+	if (freesrc)
+		free((char *)src);
+	return (dest);
+}
+
+char *ft_strchr(const char *s, int c)
+{
+	unsigned char *str;
+
+	str = (unsigned char *)s;
+	if (!s)
+	{
+		return (NULL);
+	}
+	while (*str != (unsigned char)c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	if (*str == '\n')
+		return ((char *)str);
+	return ((char *)str);
+}
+
+size_t ft_strlen(const char *str, int nextbackn)
+{
+	size_t i;
 
 	i = 0;
 	if (!str)
@@ -30,10 +78,10 @@ size_t	ft_strlen(const char *str, int nextbackn)
 	return (i);
 }
 
-size_t	ft_strlcpy(char *dest, char *src, size_t size)
+size_t ft_strlcpy(char *dest, char *src, size_t size)
 {
-	size_t	j;
-	size_t	i;
+	size_t j;
+	size_t i;
 
 	j = 0;
 	i = 0;
@@ -52,12 +100,12 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size)
 	return (j);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2, int freearg)
+char *ft_strjoin(char const *s1, char const *s2, int freearg)
 {
-	char	*str;
-	size_t	i;
-	size_t	j;
-	size_t	k;
+	char *str;
+	size_t i;
+	size_t j;
+	size_t k;
 
 	k = 0;
 	i = (int)ft_strlen((char *)s1, 1);
@@ -79,23 +127,27 @@ char	*ft_strjoin(char const *s1, char const *s2, int freearg)
 	return (str);
 }
 
-
-
-
-
-
-
-
-
-
-// 	while ((!ft_strchr(buffer, '\0') || !ft_strchr(buffer, '\n') || !(overflow)) && bytesread == BUFFER_SIZE)
+// 	while ((!ft_strchr(buffer, '\0') || !ft_strchr(buffer, '\n') || !(overflow))
+// NO_FREE,NONEXTBACKN);
+// 				temp = ft_strdup(temp, FREE, NEXTBACKN);
+// 				return (ft_strjoin(buffer, temp));
+// 			}
+// 		}
+// 		if ((!bytesread && !buffer) || (bytesread == -1))
+// 			return (temp = ft_free(NULL, temp, 2));
+// 	}
+// 	temp = ft_free(NULL, temp, 2);
+// 	return (buffer);
+// }// && bytesread == BUFFER_SIZE)
 // 	{
 // 		if (overflow)
 // 		{
-// 			if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n') + 1) != '\0')
+// 			if (ft_strchr(overflow, '\n') && *(ft_strchr(overflow, '\n')
+// + 1) != '\0')
 // 			{
 // 				temp = ft_strdup(overflow, FREE, NONEXTBACKN);
-// 				overflow = ft_strdup(ft_strchr(temp, '\n') + 1, NO_FREE,NONEXTBACKN);
+// 				overflow = ft_strdup(ft_strchr(temp, '\n') + 1,
+// NO_FREE,NONEXTBACKN);
 // 				temp = ft_strdup(temp, FREE, NEXTBACKN);
 // 				return (ft_strjoin(buffer, temp));
 // 			}
@@ -108,12 +160,15 @@ char	*ft_strjoin(char const *s1, char const *s2, int freearg)
 // 			temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 // 			bytesread = read(fd, temp, BUFFER_SIZE);
 // 			temp[bytesread] = '\0';
-// 			if (bytesread != 0 && bytesread == BUFFER_SIZE && (!(ft_strchr(temp,'\n'))))
+// 			if (bytesread != 0 && bytesread == BUFFER_SIZE
+// && (!(ft_strchr(temp,'\n'))))
 // 				buffer = ft_strjoin(buffer, temp);
-// 			else if ((ft_strchr(temp, '\n') || ft_strchr(temp, '\0'))&& bytesread != 0)
+// 			else if ((ft_strchr(temp, '\n') || ft_strchr(temp,
+// '\0'))&& bytesread != 0)
 // 			{
 // 				if (ft_strchr(temp, '\n'))
-// 					overflow = ft_strdup(ft_strchr(temp, '\n') + 1, NO_FREE,NONEXTBACKN);
+// 					overflow = ft_strdup(ft_strchr(temp, '\n') + 1,
+// NO_FREE,NONEXTBACKN);
 // 				temp = ft_strdup(temp, FREE, NEXTBACKN);
 // 				return (ft_strjoin(buffer, temp));
 // 			}
