c61d6b2e0b37febeb3bb1608ec067af68070e8e0
bastien <bastien.poyet@outlook.fr>
2023-12-19 22:52:45 +0100

    debut des fonctions de lalgo

--- push_swap/ft_get_number.c
+++ push_swap/ft_get_number.c
@@ -6,25 +6,30 @@
 /*   By: bpoyet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 16:08:47 by bpoyet            #+#    #+#             */
-/*   Updated: 2023/12/19 16:30:59 by bpoyet           ###   ########.fr       */
+/*   Updated: 2023/12/19 21:04:05 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack get_min(t_listx *lista, int range, int *pos)
-{
-    (void)range;
-    t_stack *stacka;
+// t_stack *get_min(t_listx *lista, int *range, int *pos)
+// {
+//     (void)range;
+//     t_stack *stacka;
 
-    if (lista->ptrbegin)
-    {
-        *pos = 0;
-        stacka = lista->ptrbegin;
-        while (stacka->number >= 25)
-        {
-            stacka = stacka->next;
-            *pos = *pos + 1;
-        }
-    }
-}
+//     if (lista->ptrbegin)
+//     {
+//         *pos = 0;
+//         stacka = lista->ptrbegin;
+//         while (stacka->number > 25)
+//         {
+//             stacka = stacka->next;
+//             *pos = *pos + 1;
+//         }
+//         if (stacka->number < 25)
+//             return (stacka);
+//     }
+//     return (NULL);
+// }
+
+ft_pb(lis)
