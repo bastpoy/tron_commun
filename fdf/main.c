1163be9864a7cd356d84958ca426db8f5bb82fe8
bastien <bastien.poyet@outlook.fr>
2024-01-10 14:10:20 +0100

    fdf

--- fdf/main.c
+++ fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: bpoyet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 15:19:58 by bpoyet            #+#    #+#             */
-/*   Updated: 2024/01/06 20:41:43 by bpoyet           ###   ########.fr       */
+/*   Updated: 2024/01/08 23:26:55 by bpoyet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,50 @@ void ft_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int *)dst = color;
 }
 
+size_t ft_strlen_space_backn(const char *str)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    j = 0;
+    if (!str)
+        return (0);
+    while (str[i] != '\0')
+    {
+        if (str[i] != ' ' && str[i] != '\n')
+        {
+            printf("%c \n", str[i]);
+            j++;
+        }
+        i++;
+    }
+    return (j);
+}
+
+int *ft_get_length_width()
+{
+    int fd;
+    char *str;
+    int *lengthxy = malloc(sizeof(int) * 2);
+
+    fd = open("entry.fdf", O_RDONLY);
+    str = get_next_line(fd);
+    lengthxy[0] = ft_strlen_space_backn(str);
+    lengthxy[1] = 0;
+    while (str)
+    {
+        lengthxy[1]++;
+        str = get_next_line(fd);
+    }
+    printf("lengthstr %d et widthstr %d \n", lengthxy[0], lengthxy[1]);
+    close(fd);
+    return (lengthxy);
+}
+
+// void ft_grid(void *mlx_ptr, void *mlx_win, t_data img)
+// {
+// }
 void ft_losange(void *mlx_ptr, void *mlx_win, t_data img)
 {
     int x = 80;
@@ -86,6 +130,26 @@ void ft_circle(void *mlx_ptr, void *mlx_win, t_data img)
     }
 }
 
+void ft_line(void *mlx_ptr, void *mlx_win, t_data img, double *value)
+{
+    double coef = (value[3] - value[2]) / (value[1] - value[0]);
+    double yzero = value[2] - (coef * value[0]);
+    printf("yzero %f\n", yzero);
+    int x;
+    int y;
+    int incr;
+
+    incr = 1;
+    while (value[0] <= value[1])
+    {
+        x = value[0];
+        y = (value[0] * coef) + yzero;
+        ft_mlx_pixel_put(&img, x, y, 0x00ff0000);
+        mlx_put_image_to_window(mlx_ptr, mlx_win, img.img, 0, 0);
+        value[0] = value[0] + incr;
+    }
+}
+
 int ft_get_abs(char *str)
 {
     int count;
@@ -105,8 +169,11 @@ int main(void)
 {
     void *mlx_ptr;
     void *mlx_win;
-    int fd;
-    char *str;
+    double tab[4] = {1200, 1750, 50, 600};
+    double tab1[4] = {150, 1250, 350, 850};
+    int lengthx;
+    int lengthy;
+    int *lengthxy = malloc(sizeof(int) * 2);
     t_data img;
 
     mlx_ptr = mlx_init();
@@ -114,11 +181,13 @@ int main(void)
     img.img = mlx_new_image(mlx_ptr, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-    fd = open("entry.fdf", O_RDONLY);
-    str = get_next_line(fd);
+    lengthxy = ft_get_length_width();
+    lengthx = lengthxy[0];
+    lengthy = lengthxy[1];
+    printf("%d et %d\n", lengthx, lengthy);
 
-    ft_mlx_pixel_put(&img, 1910, 1000, 0x00ff0000);
-    mlx_put_image_to_window(mlx_ptr, mlx_win, img.img, 0, 0);
+    ft_line(mlx_ptr, mlx_win, img, tab);
+    ft_line(mlx_ptr, mlx_win, img, tab1);
 
     mlx_loop(mlx_ptr);
 }
