9b8b3ab758095605a34ef8ace145304a04da78bf
bastpoy <bastien.poyet@outlook.fr>
2024-08-21 17:08:05 +0200

    re

--- m4/cpp/cpp06/ex00/ScalarConverter.cpp
+++ m4/cpp/cpp06/ex00/ScalarConverter.cpp
@@ -79,10 +79,12 @@ bool isDouble(std::string input)
 
 void doChar(std::string input)
 {
-    std::cout << "je suis dans le char" << std::endl;
     std::cout << "char: " << input << std::endl;
     std::cout << "int: " << static_cast<int>(input[0]) << std::endl;
-    std::cout << "float: " << static_cast<float>(input[0]) << std::endl;
+    std::cout << "float: " << static_cast<float>(input[0]);
+    if(input.find('.') != std::string::npos)
+        std::cout << "f";
+    std::cout << std::endl;
     std::cout << "double: " << static_cast<double>(input[0]) << std::endl;
 }
 
@@ -101,35 +103,43 @@ void doInt(std::string input)
     if(number > static_cast<double>(FLT_MAX) || number < static_cast<double>(FLT_MIN))
         std::cout << "float: overflow" << std::endl;
     else
-        std::cout << "float: " << static_cast<float>(number) << std::endl;
+    {
+        std::cout << "float: " << static_cast<float>(number);
+        if(input.find('.') != std::string::npos)
+            std::cout << "f";
+        std::cout << std::endl;
+    }
     if(number == HUGE_VAL || number == -HUGE_VAL)
         std::cout << "double: overflow" << std::endl;
     else
         std::cout << "double: " << number << std::endl;
-    std::cout << "je suis dans le int" << std::endl;
 }
 
-void doFloat(std::string input)
+void doFloat(std::string input, bool &isDiff)
 {
     double number = strtod(input.c_str(), NULL);
-
+    
     if(input.find('.') == std::string::npos && number > 32 && number < 127)
         std::cout << "char: '" << static_cast<char>(number) << "'" << std::endl;
     else
         std::cout << "char: impossible" << std::endl;
-    if(number > static_cast<double>(INT_MAX) || number < static_cast<double>(INT_MIN))
+    if(number > static_cast<double>(INT_MAX) || number < static_cast<double>(INT_MIN) || std::isnan(number))
         std::cout << "int: overflow" << std::endl;
     else
         std::cout << "int: " << static_cast<int>(number) << std::endl;        
-    if(number > static_cast<double>(FLT_MAX) || number < static_cast<double>(FLT_MIN))
+    if((number > static_cast<double>(FLT_MAX) || number < static_cast<double>(FLT_MIN)) && !isDiff)
         std::cout << "float: overflow" << std::endl;
     else
-        std::cout << "float: " << number << std::endl;
+    {
+        std::cout << "float: " << static_cast<float>(number);
+        if(input.find('.') != std::string::npos || isDiff)
+            std::cout << "f";
+        std::cout << std::endl;
+    }
     std::cout << "double: " << static_cast<double>(number) << std::endl;        
-    std::cout << "je suis dans le float" << std::endl;    
 }
 
-void doDouble(std::string input)
+void doDouble(std::string input, bool &isDiff)
 {
     double number = strtod(input.c_str(), NULL);
 
@@ -137,33 +147,57 @@ void doDouble(std::string input)
         std::cout << "char: '" << static_cast<char>(number) << "'" << std::endl;
     else
         std::cout << "char: impossible" << std::endl;
-    if(number > static_cast<double>(INT_MAX) || number < static_cast<double>(INT_MIN))
+    if(number > static_cast<double>(INT_MAX) || number < static_cast<double>(INT_MIN) || std::isnan(number))
         std::cout << "int: overflow" << std::endl;
     else
         std::cout << "int: " << static_cast<int>(number) << std::endl;
-    if(number > static_cast<double>(FLT_MAX) || number < static_cast<double>(FLT_MIN))
+    if((number > static_cast<double>(FLT_MAX) || number < static_cast<double>(FLT_MIN)) && isDiff)
         std::cout << "float: overflow" << std::endl;
     else
-        std::cout << "float: " << static_cast<float>(number) << std::endl;
-    if(number == HUGE_VAL || number == -HUGE_VAL)
+    {
+        std::cout << "float: " << static_cast<float>(number);
+        if(input.find('.') != std::string::npos || isDiff)
+            std::cout << "f";
+        std::cout << std::endl;
+    }
+    if((number == HUGE_VAL || number == -HUGE_VAL) && !isDiff)
         std::cout << "double: overflow" << std::endl;
     else
         std::cout << "double: " << number << std::endl;
-    std::cout << "Je suis dans le double" << std::endl;
+}
+
+bool isDiffFloat(std::string input, bool &isdiff)
+{
+    if(input == "-inff" || input == "+inff" || input == "nanf")
+    {
+        isdiff = true;
+        return (true);
+    }
+    return (false);
+}
+
+bool isDiffDouble(std::string input, bool &isdiff)
+{
+    if(input == "-inf" || input == "+inf" || input == "nan")
+    {
+        isdiff = true;
+        return (true);
+    }
+    return (false);
 }
 
 void ScalarConverter::convert(std::string input)
 {
+    bool isDiff = false;
+
     if(isChar(input))
         doChar(input);
     else if(isInt(input))
         doInt(input);
-    else if(isFloat(input))
-        doFloat(input);
-    else if(isDouble(input))
-        doDouble(input);
+    else if(isFloat(input) || isDiffFloat(input, isDiff))
+        doFloat(input, isDiff);
+    else if(isDouble(input) || isDiffDouble(input, isDiff))
+        doDouble(input, isDiff);
     else
-        std::cout << "Impossible convertion" << std::endl;
+        std::cout << "Impossible convertion of input number" << std::endl;
 }
-
-//detect le type
