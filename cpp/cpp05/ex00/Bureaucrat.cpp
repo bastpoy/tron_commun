960e56444ab8a66c59aceb9fd3b4126277efbcb0
bastpoy <bastien.poyet@outlook.fr>
2025-01-08 13:05:35 +0100

    refactore the structure of the repo

---
+++ cpp/cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,79 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): _name("default"), _grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade): _name(name)
+{
+    if(grade > 150)
+        throw GradeTooLowException();
+    else if(grade < 1)
+        throw GradeTooHighException();
+    this->_grade = grade;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "ilyanus" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& copy)
+{
+    *this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
+{
+    if(this != &copy)
+    {
+        this->_grade = copy.getGrade();
+    }
+    return(*this);
+}
+
+// MEMEBER FUNCTIONS
+
+std::string const Bureaucrat::getName() const
+{
+    return this->_name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->_grade;
+}
+
+void Bureaucrat::decrement()
+{
+    if(this->_grade == 150)
+        throw GradeTooLowException();
+    this->_grade++;
+}
+
+void Bureaucrat::increment()
+{
+    if(this->_grade == 1)
+        throw GradeTooHighException();
+    this->_grade--;
+}
+
+// EXCEPTION
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return("grade too low");
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return("grade too high");
+}
+
+// SURCHARGE OPERATEUR <<
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b)
+{
+    os << b.getName() << ", bureaucrat grade " << b.getGrade() << std::endl;
+    return os; 
+}
---
+++ cpp/cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,210 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <algorithm>
+#include <ctime>
+#include <limits.h>
+
+template <typename Container>
+class PmergeMe
+{
+    private:
+        Container merge;
+    public:
+        PmergeMe();
+        ~PmergeMe();
+        PmergeMe(const PmergeMe &copy);
+        PmergeMe &operator=(const PmergeMe &copy);
+
+        void sort(Container &array, int min, int max);
+        void merge_insert(Container &array, int min, int max);
+        void algorithm(Container &array, int min, int max);
+        
+        bool addNumbers(char *argv[]);
+        Container &getContainer();
+        void printArray(Container array);
+        
+};
+
+template <typename Container>
+PmergeMe<Container>::PmergeMe()
+{}
+template <typename Container>
+PmergeMe<Container>::~PmergeMe()
+{}
+template <typename Container>
+PmergeMe<Container>::PmergeMe(const PmergeMe &copy)
+{
+    *this = copy;
+}
+
+template <typename Container>
+PmergeMe<Container> &PmergeMe<Container>::operator=(const PmergeMe<Container> &copy)
+{
+    if(this != &copy)
+    {
+        this->merge = copy.merge;
+    }
+    return (*this);
+}
+
+template <typename Container>
+bool PmergeMe<Container>::addNumbers(char *argv[])
+{
+    int i = 0;
+    while(argv[i])
+    {
+        char *endptr = NULL;
+        long num = strtol(argv[i] , &endptr, 0);
+        if(num < 0)
+        {
+            std::cout << "Error: " << num << " is negative" << std::endl;
+            return (false);
+        }
+        if(num > INT_MAX)
+        {
+            std::cout << "Error: " << num << " is not an integer" << std::endl;
+            return (false);
+        }
+        if(*endptr != '\0')
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            this->merge.push_back(num);
+        }
+        i++;
+    }
+    return (true);
+}
+
+// std::vector<int> generateJacobsthal(int count) {
+//     std::vector<int> jacobsthal;
+
+//     if (count <= 0) {
+//         return jacobsthal;
+//     }
+//     jacobsthal.push_back(0);
+//     if (count == 1) {
+//         return jacobsthal;
+//     }
+//     jacobsthal.push_back(1);
+//     if (count == 2) {
+//         return jacobsthal;
+//     }
+
+//     for (int i = 2; i < count; ++i) {
+//         int next = jacobsthal[i - 1] + 2 * jacobsthal[i - 2];
+//         if(next < 0)
+//             return (jacobsthal);
+//         jacobsthal.push_back(next);
+//     }
+
+//     return jacobsthal;
+// }
+
+template <typename Container>
+void PmergeMe<Container>::sort(Container &array, int min, int max)
+{
+    if(max - min == 1)
+    {
+        if(array[min] > array[max])
+        {
+            std::swap(array[min], array[max]);
+        }
+    }
+    else if(max - min == 2)
+    {
+        if(array[min] > array[max - 1])
+        {
+            std::swap(array[min], array[max - 1]);
+        }
+        if(array[max - 1] > array[max])
+        {
+            std::swap(array[max - 1], array[max]);
+        }
+        if(array[min] > array[max - 1])
+        {
+            std::swap(array[min], array[max - 1]);
+        }
+    }
+}
+template <typename Container>
+void PmergeMe<Container>::merge_insert(Container &array, int min, int max)
+{
+    int highmiddle = ((max + min) / 2) + 1; // middle entre low et high
+    int lowmiddle = (highmiddle - 1 + min) / 2; 
+    int extrem = 0;
+
+    while(highmiddle <= max)
+    {
+        if((array[highmiddle] >= array[lowmiddle] && 
+            array[highmiddle] <= array[lowmiddle + 1]) || 
+            array[highmiddle] < array[min] ||
+            array[highmiddle] > array[highmiddle - 1])
+        {
+            int number = array[highmiddle];
+            if(array[highmiddle] >= array[lowmiddle] && array[highmiddle] <= array[lowmiddle + 1])
+            {
+                array.erase(array.begin() + highmiddle);
+                array.insert(array.begin() + lowmiddle + 1, number);    
+            }
+            else if(array[highmiddle] < array[min])
+            {
+                array.erase(array.begin() + highmiddle);
+                array.insert(array.begin() + min, number);
+            }
+            extrem = 0;
+            highmiddle++;
+            lowmiddle = (highmiddle - 1 + min) / 2;
+        }
+        else if(array[highmiddle] < array[lowmiddle])
+        {
+            lowmiddle = (lowmiddle + extrem) / 2;
+            if(lowmiddle < min)
+                lowmiddle = min;
+        }
+        else if(array[highmiddle] > array[lowmiddle + 1])
+        {
+            extrem = lowmiddle;
+            lowmiddle = (highmiddle + lowmiddle) / 2;
+        }
+    }    
+}
+
+template <typename Container>
+void PmergeMe<Container>::algorithm(Container &array, int min, int max)
+{
+    if(max - min > 2)
+    {
+        int middle = (max + min) / 2; // trouver le milieu de chaque intervalle
+        algorithm(array, min, middle); // le cote min de la division par deux
+        algorithm(array, middle + 1, max); // le cote max de la division par deux;
+        merge_insert(array, min, max);
+    }
+    else
+        sort(array, min, max);
+}
+
+template <typename Container>
+Container &PmergeMe<Container>::getContainer()
+{
+    return (this->merge);
+}
+template <typename Container>
+void PmergeMe<Container>::printArray(Container array)
+{
+    for(size_t i = 0; i < array.size(); i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
+}
+#endif
